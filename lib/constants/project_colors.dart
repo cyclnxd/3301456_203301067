373c4191a3ac33648b66a34556a530db16ec0886
cyclnxd <herakles13579@gmail.com>
2022-06-12 12:26:32 +0300

    added some constants

--- lib/constants/project_colors.dart
+++ lib/constants/project_colors.dart
@@ -4,5 +4,6 @@ abstract class ProjectColors {
   ProjectColors._();
   static const black = Color(0xFF121212);
   static const white = Color(0xFFffffff);
+  static const gray = Color(0xFFBBBBBB);
   static const textFamily = 'roboto';
 }
